fcbaacd3b57da002fa3ce0bd362b4cf768392e8d
unknown <DeshaiesRP@megavolt-dev-88.megavoltd.lan>
2014-10-16 10:36:30 -0400

    Added more specification about the support of the Google Chromecast Formatted and added more comments to the agency.js file

--- js/agency.js
+++ js/agency.js
@@ -6,34 +6,33 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+      //Scroll the page to the right section when an element on the menu is clicked
+      $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+          scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+      });
+
+      //Set all the features box to the same height
+      var maxHeightFeatureHeader = 0;
+      $(".feature-header").each(function() {
+        var $this = $(this);
+        var currentFeatureHeaderHeight = $this.height();
+
+        if (currentFeatureHeaderHeight > maxHeightFeatureHeader) {
+          maxHeightFeatureHeader = currentFeatureHeaderHeight;
+        }
+      });
+      $(".feature-header").height(maxHeightFeatureHeader);
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+      target: '.navbar-fixed-top'
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+      $('.navbar-toggle:visible').click();
     });
-
-    var maxHeightFeatureHeader = 0;
-
-    $(".feature-header").each(function(){
-      var $this = $(this);
-      var currentFeatureHeaderHeight = $this.height();
-
-      if(currentFeatureHeaderHeight > maxHeightFeatureHeader){
-        maxHeightFeatureHeader = currentFeatureHeaderHeight;
-      }
-    });
-
-    $(".feature-header").height(maxHeightFeatureHeader);
-});
-
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
