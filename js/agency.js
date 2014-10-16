2aca2ea1f629cc9a5944a75d6180c1695ce5e7ce
unknown <DeshaiesRP@megavolt-dev-88.megavoltd.lan>
2014-10-16 12:24:32 -0400

    Fixed a javascript error in agency.js

--- js/agency.js
+++ js/agency.js
@@ -1,4 +1,4 @@
-/*!
+/*
  * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
@@ -36,3 +36,4 @@ $(function() {
     $('.navbar-collapse ul li a').click(function() {
       $('.navbar-toggle:visible').click();
     });
+});
